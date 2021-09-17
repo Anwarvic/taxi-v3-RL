90d9abec316974bb09004c76d1bbdf57dc51e3ef
anwarvic <mohamedanwarvic@gmail.com>
2021-09-17 10:51:32 +0000

    added sarsa model

---
+++ sarsa.py
@@ -0,0 +1,20 @@
+from q_learning import QLearning
+
+
+class SARSA(QLearning):
+    def _get_next_reward(self, state, action, reward, next_state):
+        next_action = self.select_action(state)
+        next_reward = (reward + self.gamma*self.Q[next_state, next_action])
+        return next_reward
+
+class SARSAmax(QLearning):
+    def _get_next_reward(self, state, action, reward, next_state):
+        next_reward = reward + self.gamma*(self.Q[next_state, :] - self.Q[state, action]).max()
+        return next_reward
+
+class ExpectedSARSA(QLearning):
+    def _get_next_reward(self, state, action, reward, next_state):
+        policy = self._epsilon_greedy(next_state)
+        next_reward = reward + self.gamma*sum(policy * self.Q[next_state, :])
+        return next_reward
+    
