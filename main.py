8b887f6ed9ab24966963978d491de786c82219b4
anwarvic <mohamedanwarvic@gmail.com>
2021-09-24 00:15:52 +0000

    minor changes in main.py

--- main.py
+++ main.py
@@ -8,13 +8,12 @@ from utils import *
 if __name__ == "__main__":
     env = gym.make('Taxi-v3')
 
-    # conf = load_conf("conf.yaml")
-    # AgentModule = AgentFactory.create_agent("SARSA")
-    # agent = AgentModule(env.observation_space.n, env.action_space.n, conf)
-    # avg_rewards, best_avg_reward = interact(env, agent, conf)
-    # render(env, agent)
+    conf = load_conf("conf.yaml")
+    AgentModule = AgentFactory.create_agent("Expected-SARSA")
+    agent = AgentModule(env.observation_space.n, env.action_space.n, conf)
+    avg_rewards, best_avg_reward = train(env, agent, conf)
+    interact(env, agent)
 
-    
     #compare
-    compare(env, ["Q-learning", "SARSA", "Expected-SARSA"])
+    # compare(env, ["Q-learning", "SARSA", "Expected-SARSA"])
     #NOTE: available algorithms are: ["Q-learning", "SARSA", "Expected-SARSA"]
